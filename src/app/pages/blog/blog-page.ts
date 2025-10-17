317cf5198020a1b6d69aebccb68267e4ab09ecd7
BATMAN <Mokhetkc@hotmail.com>
2025-10-17 14:23:34 +0200

    Fixes.
    Fixes.

--- src/app/pages/blog/blog-page.ts
+++ src/app/pages/blog/blog-page.ts
@@ -4,16 +4,17 @@ import { ButtonModule } from 'primeng/button';
 import { AppFloatingConfigurator } from '../../layout/component/app.floatingconfigurator';
 import { CheckOutComponent } from '../../features/components/check-out/check-out.component';
 import { ChatComponent } from '../../features/components/chat/chat.component';
+import { BlogComponent } from '../../features/components/blog/blog.component';
 import { Card } from 'primeng/card';
 
 @Component({
-    selector: 'app-chat-page',
+    selector: 'app-blog-page',
     standalone: true,
-    imports: [ChatComponent, Card],
+    imports: [BlogComponent, Card],
     template: `
         <p-card>
-            <app-chat></app-chat>
+            <app-blog></app-blog>
         </p-card>
     `
 })
-export class ChatPage {}
+export class BlogPage {}
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -6,9 +6,13 @@ import { TradingEconomicsDashboard } from './trading-economics-dashboard/trading
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
 import { CheckOutPage } from './check-out-page/check-out-page';
+import { ChatPage } from './chat-page/chat-page';
+import { BlogPage } from './blog/blog-page';
 
 
 export default [
+    { path: 'blog', component: BlogPage },
+    { path: 'chat', component: ChatPage },
     { path: 'products', component: AllProductsComponent },
     { path: 'shopping-cart', component: ShoppingCartComponent },
     { path: 'check-out', component: CheckOutPage },
